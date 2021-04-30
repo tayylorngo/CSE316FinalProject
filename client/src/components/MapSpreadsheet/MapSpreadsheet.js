29023fb9ed3557da93f50825677eb76b81d381a5
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2021-04-30 13:09:26 -0400

    minor spreadsheet styling

--- client/src/components/MapSpreadsheet/MapSpreadsheet.js
+++ client/src/components/MapSpreadsheet/MapSpreadsheet.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import './MapSpreadsheet.css';
+import {WRow, WCol} from 'wt-frontend';
 
 const MapSpreadsheet = (props) => {
 
+    const whiteColor = {
+        color: "white",
+        fontSize: "1.5rem",
+        marginRight: "0.5%"
+    }
 
-
+    const blueColor = {
+        color: "lightblue",
+        fontSize: "1.5rem",
+        fontWeight: "bold"
+    }
 
 
     return(
-        <div>
-            <h1>{props.map.name}</h1>
+        <div id="map-spreadsheet">
+            <div id="regionName"><span style={whiteColor}>Region Name: </span><span style={blueColor}>{props.map.name}</span></div>
+            <WRow>
+                <WCol size="2">
+                    Name
+                </WCol>
+                <WCol size="2">
+                    Capital
+                </WCol>
+                <WCol size="2">
+                    Leader
+                </WCol>
+                <WCol size="2">
+                    Flag
+                </WCol>
+                <WCol size="4">
+                    Landmarks
+                </WCol>
+            </WRow>
         </div>
     );
 }
