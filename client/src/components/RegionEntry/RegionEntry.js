7aa95567bbb2885739c363472f58e204433d3079
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2021-05-12 16:19:02 -0400

    added inputs to editing subregion name, capital, and leader

--- client/src/components/RegionEntry/RegionEntry.js
+++ client/src/components/RegionEntry/RegionEntry.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './RegionEntry.css';
-import {WRow, WCol, WButton} from 'wt-frontend';
+import {WRow, WCol, WButton, WInput} from 'wt-frontend';
 
 const RegionEntry = (props) => {
+
+    const [editingName, toggleEditingName] = useState(false);
+    const [editingCapital, toggleEditingCapital] = useState(false);
+    const [editingLeader, toggleEditingLeader] = useState(false);
 
     const openNewRegion = () => {
         props.handleSetActiveMap(props.entry._id);
@@ -16,26 +20,86 @@ const RegionEntry = (props) => {
         props.handleDeleteRegion(props.entry._id);
     }
 
+    const handleEditName = () => {
+        toggleEditingName(!editingName);
+    }
+
+    const handleEditCapital = () => {
+        toggleEditingCapital(!editingCapital);
+    }
+
+    const handleEditLeader = () => {
+        toggleEditingLeader(!editingLeader);
+    }
+
+    const blackText = {
+        color: "black"
+    }
+
     return(
         <div>
             <WRow>
                 <WCol size="2" className="tableEntry subregion-name">
-                    <div className="entry-name">
-                        <span className='deleteButton'>
-                            <button onClick={handleDelete}>
-                                X
-                            </button>
-                        </span>
-                        <span className="region-name" onClick={openNewRegion}> 
-                            {props.entry.name}
-                        </span>
-                    </div>
+                    {!editingName ? 
+                            <div className="entry-name" onDoubleClick={handleEditName}>
+                                <span className='deleteButton'>
+                                    <button onClick={handleDelete}>
+                                        X
+                                    </button>
+                                </span>
+                                <span className="region-name" onClick={openNewRegion}> 
+                                    {props.entry.name}
+                                </span>
+                            </div>
+                            : 
+                        <div>
+                           <WInput
+                                wType="lined"
+                                style={blackText}
+                                className="name-input"
+                                hoverAnimation="solid"
+                                defaultValue={props.entry.name}
+                                onBlur={handleEditName}
+                                autoFocus
+                            />;
+                        </div>
+                    }
                 </WCol>
                 <WCol size="2" className="tableEntry capital-name">
-                    <div className="entry-name">{props.entry.capital}</div>
+                    {!editingCapital ? 
+                        <div className="entry-name" onDoubleClick={handleEditCapital}>
+                            {props.entry.capital}
+                        </div> :
+                    <div>
+                        <WInput
+                            wType="lined"
+                            autoFocus
+                            style={blackText}
+                            className="name-input"
+                            hoverAnimation="solid"
+                            defaultValue={props.entry.capital}
+                            onBlur={handleEditCapital}
+                        />
+                    </div>
+                }
                 </WCol>
                 <WCol size="2" className="tableEntry leader-name">
-                    <div className="entry-name">{props.entry.leader}</div>
+                    {!editingLeader ? 
+                        <div className="entry-name" onDoubleClick={handleEditLeader}>
+                            {props.entry.leader}
+                        </div> : 
+                        <div>
+                            <WInput
+                                wType="lined"
+                                autoFocus
+                                style={blackText}
+                                className="name-input"
+                                hoverAnimation="solid"
+                                defaultValue={props.entry.leader}
+                                onBlur={handleEditLeader}
+                            />
+                        </div>
+                    }
                 </WCol>
                 <WCol size="2" className="tableEntry flag">
                     <div className="entry-name">{props.entry._id}</div>
